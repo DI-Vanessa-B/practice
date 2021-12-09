107de3c7bfa81f7f79b88e98ae577f7b23663e80
Vanessa <vanessa.buchmann@stud.hslu.ch>
2021-12-09 13:27:45 +0100

    axios installed

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,7 @@
   <h1>{{title}}</h1>
     <div class="grid-list">
     <figure v-for="image, index in images" :key="index" :class="image.size">
-      <p>{{image}}</p>
       <img :src="`/img/${image.path}`" alt="">
-      <p>{{image.size}}, {{image.hasChildren}}</p>
     </figure>
   </div>
 </div>
@@ -16,7 +14,7 @@ const images = require('~/assets/data/images.json')
 export default {
   data() {
     return{
-      title: 'Welcome',
+      title: 'Welcome to the front page',
       images: images,
     }
   }
@@ -30,23 +28,20 @@ h1 {
 }
 
 .grid-list{
-  @apply grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-12
+  @apply grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3
 }
 
 figure.small {
-  border: 10px solid red;
   width: 100%;
   column-span: 2;
 }
 
 figure.medium {
-  border: 10px solid blue;
   width: 100%;
   column-span: 4;
 }
 
 figure.large {
-  border: 10px solid yellow;
   width: 100%;
 }
 
