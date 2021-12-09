fa504bd86e993e0bff999ac23c264ca6c7dd16f8
Vanessa <vanessa.buchmann@stud.hslu.ch>
2021-12-09 08:53:27 +0100

    test file with images

---
+++ layout/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/">Contact</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+        <nuxt/>
+    </main>
+    
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,53 @@
 <template>
-  <Tutorial/>
+<div class="wrapper">
+  <h1>{{title}}</h1>
+    <div class="grid-list">
+    <figure v-for="image, index in images" :key="index" :class="image.size">
+      <p>{{image}}</p>
+      <img :src="`/img/${image.path}`" alt="">
+      <p>{{image.size}}, {{image.hasChildren}}</p>
+    </figure>
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+const images = require('~/assets/data/images.json')
+export default {
+  data() {
+    return{
+      title: 'Welcome',
+      images: images,
+    }
+  }
+}
 </script>
+
+<style>
+h1 {
+  font-size: 3vh;
+  font-weight: bold;
+}
+
+.grid-list{
+  @apply grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-12
+}
+
+figure.small {
+  border: 10px solid red;
+  width: 100%;
+  column-span: 2;
+}
+
+figure.medium {
+  border: 10px solid blue;
+  width: 100%;
+  column-span: 4;
+}
+
+figure.large {
+  border: 10px solid yellow;
+  width: 100%;
+}
+
+</style>
